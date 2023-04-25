97bbf14bebc391c3a3b586ab2a1349b039e618a3
jack <jack11jack11jack@gmail.com>
2023-04-25 15:52:29 +0800

    1.移除追蹤out

--- out/utils/dart/dart_helprt.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import * as _ from "lodash";
-// import { getPubspec } from "./pubspec/get_pubspec";
-// export async function hasDependency(dependency: string) {
-//   const pubspec = await getPubspec();
-//   const dependencies = _.get(pubspec, "dependencies", {});
-//   return _.has(dependencies, dependency);
-// }
-//# sourceMappingURL=dart_helprt.js.map
